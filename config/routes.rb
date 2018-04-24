3301fbb216bcc0bb9692709020e860d6f621b946
mrkagelui <jasonlv@live.com>
2018-04-24 22:11:40 +0800

    get_friends spec and implementation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json}, path: '/api' do
     scope module: :v1, constraints: Constraint::Api_constraints.new(version: 1, default: true) do
       post 'add_friend', to: 'friendships#create'
+      get 'get_friends', to: 'friendships#get_friends'
     end
   end
 end
