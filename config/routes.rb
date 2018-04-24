eaed69a959f2d47bee6ac340a7d70c3c5dea5471
mrkagelui <jasonlv@live.com>
2018-04-24 21:47:45 +0800

    add_friend spec and implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json}, path: '/api' do
+    scope module: :v1, constraints: Constraint::Api_constraints.new(version: 1, default: true) do
+      post 'add_friend', to: 'friendships#create'
+    end
+  end
 end
